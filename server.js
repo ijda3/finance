7be34e493adf9dd8992937e9d37b7f53f5111598
Itamar Jr <itamar.jda@gmail.com>
2021-04-17 19:53:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.listen(5000, () => {
-	console.log("http://localhost:5000/");
+app.listen(port, () => {
+	console.log(`http://localhost:${port}/`);
 });
