60ceb40560b4c6a46ce5015ae42faeac26a3e765
Itamar Jr <itamar.jda@gmail.com>
2021-04-18 10:19:16 -0300

    Fix indentation

--- server.js
+++ server.js
@@ -13,28 +13,28 @@ app.disable("view cache");
 app.use(express.static(path.join(__dirname, "./static")));
 
 app.get("/", function (req, res) {
-	res.render("index", {
-		menu_conta: true,
-	});
+  res.render("index", {
+    menu_conta: true,
+  });
 });
 
 app.get("/:page", function (req, res) {
-	const { page } = req.params;
-	const CREDIT_CARD_PAGES = [
-		"credit-card",
-		"new-creditcard",
-		"new-transaction-creditcard",
-	];
+  const { page } = req.params;
+  const CREDIT_CARD_PAGES = [
+    "credit-card",
+    "new-creditcard",
+    "new-transaction-creditcard",
+  ];
 
-	res.render(page, {
-		credit_card_page: CREDIT_CARD_PAGES.includes(page),
-		chart_page: page == "charts",
-		menu_conta: page != "charts" && !CREDIT_CARD_PAGES.includes(page),
-		menu_credit_card: CREDIT_CARD_PAGES.includes(page),
-		menu_charts: page == "charts",
-	});
+  res.render(page, {
+    credit_card_page: CREDIT_CARD_PAGES.includes(page),
+    chart_page: page == "charts",
+    menu_conta: page != "charts" && !CREDIT_CARD_PAGES.includes(page),
+    menu_credit_card: CREDIT_CARD_PAGES.includes(page),
+    menu_charts: page == "charts",
+  });
 });
 
 app.listen(port, () => {
-	console.log(`http://localhost:${port}/`);
+  console.log(`http://localhost:${port}/`);
 });
